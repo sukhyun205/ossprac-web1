cc8bcaf24303649ea56e54869b0ad7af87bd5c90
Sukhyun Kwon <sukhyun205@gmail.com>
2021-12-02 14:15:49 +0900

    211202 server.py main.html login.html미완

--- app/server.py
+++ app/server.py
@@ -3,28 +3,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def student():
+def main():
    return render_template('main.html')
 
-@app.route('/detail', methods = ['POST', 'GET'])
-def detail():
+@app.route('/login', methods = ['GET','POST'])
+def login():
+   error = None
    if request.method == 'POST':
-      result = dict()
-      result['Name'] = request.form.get('Name')
-      result['StudentNumber'] = request.form.get('StudentNumber')
-      result['Gender'] = request.form.get('Gender')
-      result['Major'] = request.form.get('Major')
-      return render_template("detail.html",result = result)
+      if request.form['username'] != 'ossp' or request.form['password'] != 'ossp1234': #error 변수에 'Error 메시지 내용'을 대입
+         error = 'Incorrect authentication credentials! Please try again. '
+      else:
+         return render_template('application_form.html')
 
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = dict()
-      result['Name'] = request.form.get('Name')
-      result['StudentNumber'] = request.form.get('StudentNumber')
-      result['Gender'] = request.form.get('Gender')
-      result['Major'] = request.form.get('Major')
-      return render_template("result.html",result = result)
+   return render_template("login.html", error = error)
+
+
+@app.route('/submit', methods = ['POST', 'GET'])
+def submit():
+   uname = request.form.get('uname')
+   umajor = request.form.get('umajor')
+   grade = request.form.get('grade')
+
+   return render_template('submit.html', umajor=umajor, grade=grade, uname=uname)
+
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
