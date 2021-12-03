899297b0be4e30e18009367caab88c122e88de06
Sukhyun Kwon <sukhyun205@gmail.com>
2021-12-03 17:11:46 +0900

    211203 add

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+   return render_template('main.html')
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+   error = None
+   if request.method == 'POST':
+      if request.form['username'] != 'ossp' or request.form['password'] != 'ossp1234': #error 변수에 'Error 메시지 내용'을 대입
+         error = 'Incorrect authentication credentials! Please try again. '
+      else:
+         return render_template('application_form.html')
+
+   return render_template("login.html", error = error)
+
+
+@app.route('/submit', methods = ['POST', 'GET'])
+def submit():
+   uname = request.form.get('uname')
+   umajor = request.form.get('umajor')
+   grade = request.form.get('grade')
+
+   return render_template('submit.html', umajor=umajor, grade=grade, uname=uname)
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", debug=True, port=80)
